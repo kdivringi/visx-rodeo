4730712ad30737e30aa80a887ae26ad7ceaeffbd
Kaan Divringi <kdivringi@gmail.com>
2021-03-20 11:03:03 -0700

    Have the ticks located right, not labelled right

--- src/CalendarPlot.tsx
+++ src/CalendarPlot.tsx
@@ -2,7 +2,11 @@ import { eachWeekOfInterval } from 'date-fns'
 import React, { useEffect } from 'react';
 import { HeatmapRect } from '@visx/heatmap';
 import { getSeededRandom, genBins } from '@visx/mock-data';
-import { scaleOrdinal, scaleTime, scaleLinear } from '@visx/scale';
+import { scaleOrdinal, scaleTime, scaleLinear, scalePoint } from '@visx/scale';
+import { Group } from '@visx/group'
+import { AxisTop, AxisRight } from '@visx/axis'
+import * as time from 'd3-time'
+import * as array from 'd3-array';
 
 const [minDate, maxDate] = [new Date(2021, 1, 7), new Date(2021, 2, 27)]
 const weekStarts = eachWeekOfInterval({ start: minDate, end: maxDate })
@@ -12,25 +16,40 @@ const binData = genBins(
   /* length = */ weekStarts.length,
   /* height = */ 7,
   /** binFunc */ idx => 150 * idx,
-  /** countFunc */ (i, number) => 25 * (number - i) * seededRandom(),
+  /** countFunc */(i, number) => 25 * (number - i) * seededRandom(),
 );
 
+function midPoints(scale: ReturnType<typeof scaleLinear>, bins: number): number[] {
+  const ticks = scale.ticks(bins);
+  delete ticks[ticks.length - 1];
+  const interval = ticks[1] - ticks[0];
+  return ticks.map(t => t + 0.5*interval);
+}
 
+/**
+ * Histogram scales are just the column and row index -> position
+ */
 
 export default function CalendarPlot({ width, height }: { width: number, height: number }) {
-    const squareEdge = Math.min(width, height);
-    // const xScale = scaleTime({ domain: [minDate, maxDate], range: [0, width] })
-    const xScale = scaleLinear({ domain: [0, weekStarts.length], range: [0, squareEdge] })
-    // const yScale = scaleOrdinal({ domain: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], range: [0, height] })
-    const yScale = scaleLinear({ domain: [0, 7], range: [0, squareEdge] })
-    const colorScale = scaleLinear({ domain: [0, 25], range: ['#efedf5', '#bcbddc', '#756bb1'] });
-    // useEffect(() => {
-    //     console.log(binData, weekStarts)
-    // }, [])
-    return (
-        <svg height={squareEdge} width={squareEdge}>
-            {/* <rect x={0} y={0} width={width} height={height} fill="wheat"/> */}
-            <HeatmapRect data={binData} binWidth={squareEdge / 7} binHeight={squareEdge / 7} xScale={d => xScale(d)} yScale={d => yScale(d)} colorScale={colorScale} />
-        </svg>
+  const margin = { top: 25, right: 25 }
+  const squareEdge = Math.min(width - margin.right, height - margin.top);
+  const xLabelScale = scaleTime({ domain: [minDate, maxDate], range: [0, squareEdge] })
+  // const xLabelScale = scalePoint({ domain: time.timeWeeks(minDate, maxDate), range: [0, squareEdge] })
+  const xScale = scaleLinear({ domain: [0, weekStarts.length], range: [0, squareEdge] })
+  // const yScale = scaleOrdinal({ domain: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'], range: [0, height] })
+  const yScale = scaleLinear({ domain: [0, 7], range: [0, squareEdge] })
+  const colorScale = scaleLinear({ domain: [0, 25], range: ['#efedf5', '#bcbddc', '#756bb1'] });
+  // useEffect(() => {
+  //     console.log(weekStarts)
+  // }, [])
+  return (
+    <svg height={squareEdge + margin.top} width={squareEdge + margin.right}>
+      {/* <rect x={0} y={0} width={width} height={height} fill="wheat"/> */}
+      <Group top={margin.top}>
+        <HeatmapRect data={binData} binWidth={squareEdge / 7} binHeight={squareEdge / 7} xScale={d => xScale(d)} yScale={d => yScale(d)} colorScale={colorScale} />
+      </Group>
+      <AxisTop top={margin.top} scale={xScale} tickValues={midPoints(xScale, weekStarts.length)}/>
+      <AxisRight top={margin.top} left={squareEdge} scale={yScale} tickValues={midPoints(yScale, 7)}/>
+    </svg>
   );
 }
